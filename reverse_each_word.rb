2c8e53b913c02fe749320204da96c1b99142497d
Brian Vehige <brianvehige@gmail.com>
2020-02-19 08:33:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
-def reverse_each_word(sentence)
+def reverse_each_word1(sentence)
   array = sentence.split
   reversed_array = array.each {|word| word.reverse!}
   reversed_array.join(" ")
 end
 
-def reverse_each_word(sentence)
+def reverse_each_word2(sentence)
   array = sentence.split
   array.collect {|word| word.reverse!}
 end
